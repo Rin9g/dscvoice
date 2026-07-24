544253074c4c5ec754069a7a1ddf9eb53ad679f1
Rin9g <117325870+Rin9g@users.noreply.github.com>
2026-07-24 19:27:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from keep_alive import keep_alive
 status = "online"  # online/dnd/idle
 
 GUILD_ID = 1433710108373815319
-CHANNEL_ID = 1530184270851604671
+CHANNEL_ID = 1530189706598416565
 SELF_MUTE = True
 SELF_DEAF = False
 
